9c21ead09f42037d5ff180e535a5d0d75be5c70f
kartiknayak1216 <nayakkartik1216@gmail.com>
2025-01-07 21:32:30 +0530

    third commit

--- src/app/(group)/group/[groupname]/analytics/page.tsx
+++ src/app/(group)/group/[groupname]/analytics/page.tsx
@@ -1,19 +1,18 @@
-
 import { Skeleton } from "@/components/ui/skeleton";
 import React, { Suspense } from "react";
 import Periodselection from "./periodselection";
 import { GetPeriod, getStatsForDay } from "./server";
 import Statsday from "./stats";
 
-type Props={
-  params:{
-    month:string,
-    year:string,
-    groupname:string
-  }
-}
-export default async function Page({params
-}: Props) {
+type Props = {
+  params: {
+    month?: string;
+    year?: string;
+    groupname: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
   const current = new Date();
   const { month, year, groupname } = params;
 
@@ -23,15 +22,15 @@ export default async function Page({params
   };
 
   if (!groupname) {
-    <div className="min-h-screen flex items-center justify-center">
-      <span className="text-lg font-medium text-red-500">Error loading data!</span>
-    </div>
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="text-lg font-medium text-red-500">Error loading data!</span>
+      </div>
+    );
   }
 
   return (
     <div className="flex flex-1 flex-col h-full p-6 bg-gray-50 dark:bg-gray-900">
-
-
       <div className="flex justify-between items-center border-b pb-4 mb-6 border-gray-200 dark:border-gray-700">
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Analytics</h1>
         <Suspense fallback={<Skeleton className="w-[180px] h-[40px]" />}>
@@ -58,21 +57,19 @@ async function PeriodSelection({ selected }: { selected: Period }) {
   if (!periods) {
     return null;
   }
-  return (
-    <div>
-      <Periodselection period={periods} selected={selected} />
-    </div>
-  );
+  return <Periodselection period={periods} selected={selected} />;
 }
 
-async function StaticCard({ selected, groupname }: { selected: Period, groupname: string }) {
-  const periods = await getStatsForDay(selected, groupname);
-  if (!periods) {
-    return null;
+async function StaticCard({ selected, groupname }: { selected: Period; groupname: string }) {
+  const stats = await getStatsForDay(selected, groupname);
+  if (!stats) {
+    return (
+      <div className="text-red-500">Error fetching statistics for the selected period.</div>
+    );
   }
   return (
     <div className="flex flex-col gap-6 bg-white dark:bg-gray-800 p-4 rounded-md shadow-md">
-      <Statsday stats={periods} />
+      <Statsday stats={stats} />
     </div>
   );
 }
